e50b5f45537abd8ac186c1b11aba7a4e5297832c
Gabriela Menacho <menacho.gabriela@gmail.com>
2025-09-02 08:49:29 -0300

    learn(ap08): encerra secao 2.3

---
+++ script.sql
@@ -0,0 +1,16 @@
+
+
+-- DO $$
+-- BEGIN
+--     --expressão com placeholder
+--     RAISE NOTICE '% + % = %', 6, 7, 6+7 ;
+-- END;
+-- $$
+
+-- DO $$
+-- BEGIN
+--     --para exibir valores no console
+--     RAISE NOTICE 'Meu primeiro bloquinho anônimo';
+-- END;
+-- $$
+
